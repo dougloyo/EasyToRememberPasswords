4664807aa47371ebeec1077e7dbedf3a5af53bff
Douglas Loyo <douglasloyo@gmail.com>
2013-11-20 18:37:13 -0600

    Adding base requirement functionality.

--- EasyToRememberPasswords/Controllers/HomeController.cs
+++ EasyToRememberPasswords/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using EasyToRememberPasswords.Models;
 
 namespace EasyToRememberPasswords.Controllers
 {
@@ -13,8 +14,26 @@ namespace EasyToRememberPasswords.Controllers
 
         public ActionResult Index()
         {
-            return View();
+            var model = new PasswordViewModel{};
+
+            prepareModel(model);
+
+            return View(model);
         }
 
+        [HttpPost]
+        public ActionResult Index(PasswordViewModel model)
+        {
+            prepareModel(model);
+
+            model.Password = "C@briol3t";
+
+            return View(model);
+        }
+
+        private void prepareModel(PasswordViewModel model)
+        {
+            model.AvailableCharacterCount = new List<int> {5, 6, 7, 8, 9};
+        }
     }
 }
---
+++ EasyToRememberPasswords/Models/PasswordViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EasyToRememberPasswords.Models
+{
+    public class PasswordViewModel
+    {
+        public List<int> AvailableCharacterCount { get; set; }
+        public int SelectedCharacterCount { get; set; }
+        public string OriginalWord { get; set; }
+        public string Password { get; set; }
+    }
+}
